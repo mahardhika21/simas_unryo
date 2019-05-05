98889cd8ab1dc74f280797ce81410854d7835812
mahardhika21 <dboyscoz@gmail.com>
2019-05-06 00:57:15 +0700

    addd set login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Routing\UrlGenerator;
+use App\Model\User;
 
 
 class UserController extends Controller
@@ -26,5 +27,35 @@ class UserController extends Controller
 			);
 
 		return view('login', $data);
+	}
+
+
+	public function set_login(Request $request)
+	{
+		$username  = $request->input('username');
+		$password  = md5($request->input('password'));
+
+		try
+		{
+			$data = User::where('username',$username)
+						->where('password',$password)
+						->get();
+			if(!empty($data))
+			{
+				$arr_ses = array
+							(
+								"username" => $username,
+								"level"    => $data->level,
+							);
+				$request->session()->put($arr_ses);
+
+				return redirect($level);
+			}else{
+				$message = array('status' => "danger", 'message' => 'login gagal');
+				return redirect('login')->with($message);
+			}
+		}catch(Exception $e){
+			echo "".$e->message;
+		}
 	}
 }
