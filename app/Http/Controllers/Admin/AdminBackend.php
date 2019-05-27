0733cde36ae38a45e88179b69658033d1e411944
mahardhika <mahardhika894@gmail.com>
2019-05-28 06:18:32 +0700

    pix

--- app/Http/Controllers/Admin/AdminBackend.php
+++ app/Http/Controllers/Admin/AdminBackend.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB:
+use Illuminate\Support\Facades\Storages;
 use App\Model\User;
 use App\Model\Kamar;
+
 
 class AdminBackend extends Controller
 {
@@ -65,5 +67,50 @@ class AdminBackend extends Controller
 			{
 				die("File did not upload: ". $e->getMessage());
 			}
+	}
+
+
+
+	// update data kamar \\
+
+	public function update_data_kamar(Request $request,$id_kamar)
+	{
+		$sessi = $request->session()->get('roleAuth');
+
+		try
+		{
+			DB::beginTransaction();
+
+				$arr_data = array
+						(
+							"nomor_kamar"      => $request->input('lantai_kamar'),
+							"id_admin"         => $sessi['username'],
+							"lantai_kamar"     => $request->input('lantai_kamar'),
+							"foto_kamar"       => $file_name,
+							"harga_perbulan"   => $request->input('harga_perbulan'),
+							"status_kamar"     => $request->input('status_kamar'),
+						);
+
+						Kamar::where('id_kamar')->update($arr_data);
+						DB::commit();
+						redirect('admin/kamar');
+
+		}catch(Exception $e)
+		{
+				   return redirect('admin/kamar')->with(['error' => $e->getMessage()]);
+		}
+	}
+
+
+
+	public function update_foto_kamar(Request $request, $id_kamar)
+	{
+		try {
+			$room = Kamar::where('id_kamar')->get();
+			
+			
+		} catch (Exception $e) {
+			
+		}
 	}
 }
